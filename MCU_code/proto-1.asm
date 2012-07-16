41445ace97f64e7f35e54c9255f2df87ca4e921a
Gerald Nilles <geraldniles@gmail.com>
2012-07-15 23:42:27 -0500

    Update master

--- MCU_code/proto-1.asm
+++ MCU_code/proto-1.asm
@@ -11,16 +11,32 @@ SetCounter:		; This section will set up a 1 second time clock
 ; Start Counter
 
 Loop:
-; Put Device to sleep
+SLEEP
 RJMP Loop
 
 
-Interrupt:		; THis interrupt will be called every second
-; Compare Counter and CountTo
-; if Counter > CountTo
-; Toggle PortA
-;
+Interrupt:		      ; THis interrupt will be called every second
+CLEAR CountReg      ; Clear the HW Counter Register
+ADD Counter 1       ; Add to slow counter
+CMP Counter CountTo ; Compare Counter to Count-To Value
+BRGT Toggle         ; If Counter is Greater, Jump to Toggle Routine
+RETURN              ; Else Return
 
+Toggle:
+CLEAR Counter
+CMPI CountTo OnTime ; See which count-to expired
+BREQ TurnOff
+RJMP TurnOn
+
+TurnOff:
+SETI PORTA 0x00
+SETI CountTo OffTime
+RETURN
+
+TurnOn:
+SETI PORTA 0xFF
+SETI CountTo OnTime
+RETURN
 
 
 
