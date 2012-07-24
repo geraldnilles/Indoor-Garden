bff4c62b8926436c8078813d4c72978c169d4371
Gerald Nilles <geraldniles@gmail.com>
2012-07-24 07:53:14 -0500

    Update MCU_code/proto-1.asm

--- MCU_code/proto-1.asm
+++ MCU_code/proto-1.asm
@@ -1,42 +1,53 @@
 .DEF PORTA = 0x00 	; Set PORTA as the portA address
 .DEF Counter = R17	; Assign Counter to R17
 .DEF CountTo = R18	; Assign Count To Value to R18
-.DEF OnTime = 60
-.DEF OffTime = 255
+.DEF OnTime = 60    ; 
+.DEF OffTime = 250  ;
 
+.CSEG               ; Code-Segment starts here
+.ORG 0000           ; Address is zero
+RJMP Start          ; The reset-vector on Address 0000
+RETI               ; 0001: first Int-Vektor, INT0 service routine, SInce its not used, do the RETI command
+...
+
+Start:
 SetCounter:		; This section will set up a 1 second time clock
 
 ; Select a 1 second clock
 ; Enable Counter Interrupt
 ; Start Counter
 
-Loop:
-SLEEP
-RJMP Loop
+MainLoop:
+LDI CountTo OnTime  ; Load the CountTo register with the On TIme
+RCALL TurnOn         ; Turn On the GPIO
+RCALL MySleep       ; Call Sleep Subroutine
+LDI CountTo OffTime ; Load the CountTo Register with the Off Time
+RCALL TurnOff        ; Turn Off the GPIO
+RCALL MySleep       ; Call the sleep Subroutine
+RJMP MainLoop       ; Return to the beginning
 
+MySleep:
+LDI Counter 0       ; Initialize the counter
+CMP CountTo Counter ; Compare Counter and CountTo
+BRGT -2             ; If the counter is less, jump back to the Compare function
+RET                 ; Else, Return from the sleep function
 
-Interrupt:		      ; THis interrupt will be called every second
-CLEAR CountReg      ; Clear the HW Counter Register
-ADD Counter 1       ; Add to slow counter
-CMP Counter CountTo ; Compare Counter to Count-To Value
-BRGT Toggle         ; If Counter is Greater, Jump to Toggle Routine
-RETURN              ; Else Return
+; THis interrupt will be called every second
+IntCOMPA:
+IN R16,SREG         ; read status register
+PUSH R16            ; and put on stack
+INC Counter         ; Increment the Counter Register
+POP R16             ; get previous flag register from stack
+OUT SREG,R16        ; restore old status
+RETI                ; and return from int
 
-Toggle:
-CLEAR Counter
-CMPI CountTo OnTime ; See which count-to expired
-BREQ TurnOff
-RJMP TurnOn
 
 TurnOff:
 SETI PORTA 0x00
-SETI CountTo OffTime
-RETURN
+RET
 
 TurnOn:
 SETI PORTA 0xFF
-SETI CountTo OnTime
-RETURN
-
+RET
 
 
